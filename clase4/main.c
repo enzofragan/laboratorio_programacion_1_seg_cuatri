4050312dde89b529cb445c7199fbee5976a95fe1
enzofragan <enzofragan@gmail.com>
2018-09-06 11:15:22 -0300

    clase 4

--- clase4/main.c
+++ clase4/main.c
@@ -3,6 +3,14 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int miVector[5]={1,2,3,4,5};
+    int i;
+
+    for(i=0;i<5;i++)
+    {
+        printf("%d\n",miVector[i]*2);
+    }
+
+
     return 0;
 }
