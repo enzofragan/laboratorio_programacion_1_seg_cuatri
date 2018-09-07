a7afccee4b46cf542a86eabec8b16613f02af2b3
enzofragan <enzofragan@gmail.com>
2018-09-07 12:27:39 -0300

    clase 6

--- clase6_01/main.c
+++ clase6_01/main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main()
 {
-    printf("Hello world!\n");
+    char nombre[20];
+    int cantidad;
+
+    printf("ingrese un nombre: ");
+    scanf("%[^\n]",nombre);
+    /*fgets(nombre,20,stdin);
+
+    puts(nombre);
+
+    nombre[strlen(nombre)-1]='\0';*/
+
+    cantidad = strlen(nombre);
+
+    printf("cantidad %d",cantidad);
+
     return 0;
 }
