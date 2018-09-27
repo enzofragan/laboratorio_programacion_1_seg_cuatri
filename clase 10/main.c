ac376d588543ca2803bc867d58874d263861692f
enzofragan <enzofragan@gmail.com>
2018-09-27 11:58:57 -0300

    clase 10

--- clase 10/main.c
+++ clase 10/main.c
@@ -1,29 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-typedef struct
+/*typedef struct
 {
     int dia;
     int mes;
     int anio;
-}eFecha;
+}eFecha;*/
+
+typedef struct
+{
+    int id;
+    char nombre[30];
+    int duracion;
+    char profesor[30];
+}eCurso;
 
 typedef struct
 {
     int legajo;
     char nombre[50];
     float promedio;
-    eFecha fechaNacimiento;
 }eAlumno;
+
+void iniciarCurso(eCurso* ,int );
+void mostrarCurso(eCurso);
+void mostrarTodosCursos(eCurso [],int );
 
 int main()
 {
     eAlumno miAlumno={450,"enzo",5};
-    miAlumno.fechaNacimiento.dia=10;
-    miAlumno.fechaNacimiento.mes=7;
-    miAlumno.fechaNacimiento.anio=2000;
 
-    printf("%d----%s----%f---%d/%d/%d",miAlumno.legajo,miAlumno.nombre,miAlumno.promedio,miAlumno.fechaNacimiento.dia,miAlumno.fechaNacimiento.mes,miAlumno.fechaNacimiento.anio);
+    printf("%d----%s----%f",miAlumno.legajo,miAlumno.nombre,miAlumno.promedio);
 
     return 0;
+}
+
+void iniciarCurso(eCurso* curso,int tam)
+{
+    int id[3]={100,101,102};
+    char nombre[30][3]={"php","c#","vb"};
+    int duracion[3]={3,5,6};
+    char profesor[30][3]={"cristian","octavio","maxi"};
+    int i;
+
+    for(i=0;i<3;i++)
+    {
+        curso[i].id=id[i];
+        curso[i].duracion=duracion[i];
+        strcpy(curso[i].nombre,nombre[i]);
+        strcpy(curso[i].profesor,profesor[i]);
+    }
+}
+
+void mostrarCurso(eCurso curso)
+{
+    printf("%d---%s----%d-----%s",curso.id,curso.nombre,curso.duracion,curso.profesor);
+}
+
+void mostrarTodosCursos(eCurso cursos[],int tam)
+{
+    int i;
+
+    for(i=0;i<tam;i++)
+    {
+        mostrarCurso(cursos[i]);
+    }
 }
