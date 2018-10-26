99d322f1eb6c71d0675c65e6d481a34488833d66
enzofragan <enzofragan@gmail.com>
2018-10-26 12:14:56 -0300

    clase 19

--- clase_19/main.c
+++ clase_19/main.c
@@ -3,9 +3,18 @@
 
 int main()
 {
+    char p1[20];
+    char p2[20];
     FILE* pArchivo;
-    pArchivo=fopen("texto.txt","w");
-    fprintf(pArchivo,"hola :3");
+    pArchivo=fopen("texto.txt","r");
+/////////////escritura/////////////////
+    /*fprintf(pArchivo,"hola :3\n");
+    fprintf(pArchivo,"y chau \n");*/
+/////////////lectura/////////////////
+    fgets(p1,20,pArchivo);
+    puts(p1);
+    fgets(p2,20,pArchivo);
+    puts(p2);
     fclose(pArchivo);
     return 0;
 }
